8c6b804cfb82a6744c21103eeef8bbf9f9f6b9f9
johannes karoff <johannes@karoff.net>
2021-12-01 16:15:33 +0100

    prepare npm publish

---
+++ publish.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cd "$(dirname $0)"
+
+rm -rf ./npm/bin/
+mkdir ./npm/bin/
+
+sbt lambdaServer/fullOptJS::webpack
+
+cp ./lambda-server/target/scala-2.13/scalajs-bundler/main/lambda-server.js ./npm/bin/
+
