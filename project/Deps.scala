ff1b55f7e508d27f115a9868be1dfc7d94b1eaa6
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-01-28 23:43:17 +0100

    Update cats-effect to 3.3.5 (#24)

--- project/Deps.scala
+++ project/Deps.scala
@@ -7,7 +7,7 @@ object Deps {
   val scalatest = s("org.scalatest" %%% "scalatest" % "3.2.11")
 
   val cats = new {
-    val effect = s("org.typelevel" %%% "cats-effect" % "3.3.4")
+    val effect = s("org.typelevel" %%% "cats-effect" % "3.3.5")
   }
 
   val awsLambdaJS = s("net.exoego" %%% "aws-lambda-scalajs-facade" % "0.11.0")
