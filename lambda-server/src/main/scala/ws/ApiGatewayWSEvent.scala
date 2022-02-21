26ead184368be941a5f4210409a7dd329682f32f
johannes karoff <johannes@karoff.net>
2022-02-21 19:13:23 +0100

    remove unused facade code

--- lambda-server/src/main/scala/ws/ApiGatewayWSEvent.scala
+++ lambda-server/src/main/scala/ws/ApiGatewayWSEvent.scala
@@ -22,34 +22,11 @@ trait APIGatewayWSRequestContext extends js.Object {
   def requestTimeEpoch: Double             = js.native
   def routeKey: String                     = js.native
 }
-object APIGatewayWSRequestContext {
-  implicit class Ops(val self: APIGatewayWSRequestContext) extends AnyVal {
-    def authorizerWithUser: Option[APIGatewayAuthorizer] =
-      if (self.authorizer.sub.isDefined) Some(self.authorizer.asInstanceOf[APIGatewayAuthorizer]) else None
-  }
-}
 
 @js.native
 trait APIGatewayAuthorizerBase extends js.Object {
   def principalId: String     = js.native
   def sub: js.UndefOr[String] = js.native
-}
-
-@js.native
-trait APIGatewayAuthorizer extends js.Object {
-  def principalId: String = js.native
-  def sub: String         = js.native
-  def iss: String         = js.native
-  def version: String     = js.native
-  def client_id: String   = js.native
-  def event_id: String    = js.native
-  def token_use: String   = js.native
-  def scope: String       = js.native
-  def auth_time: String   = js.native
-  def exp: String         = js.native
-  def iat: String         = js.native
-  def jti: String         = js.native
-  def username: String    = js.native
 }
 
 @js.native
