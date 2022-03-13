584c2f2af9d213017e065de5980cd22aee0a0eaf
johannes karoff <johannes@karoff.net>
2022-03-13 19:30:58 +0100

    silence oidc warnings

--- cli/webpack.config.js
+++ cli/webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack');
+const path = require('path');
 const fs = require('fs');
 
 module.exports = require('./scalajs.webpack.config');
@@ -7,7 +8,10 @@ module.exports.output.filename = "fun-stack-local.js";
 module.exports.target = "node";
 module.exports.plugins = module.exports.plugins || [];
 module.exports.plugins.push(function () {
-  this.plugin('beforeRun', () => fs.copyFileSync("../../../../../oidc-server/index.js", "./oidc-server.js"))
+  this.plugin('beforeRun', () => {
+    fs.copyFileSync("../../../../../oidc-server/index.js", "./oidc-server.js");
+    fs.copyFileSync("../../../../../oidc-server/silent-attention.js", "./silent-attention.js");
+  })
 });
 module.exports.plugins.push(new webpack.BannerPlugin({
   banner: '#!/usr/bin/env -S node --enable-source-maps',
@@ -16,6 +20,10 @@ module.exports.plugins.push(new webpack.BannerPlugin({
 module.exports.plugins.push(function () {
   this.plugin('done', () => fs.chmodSync('fun-stack-local.js', '755'))
 });
+module.exports.plugins.push(new webpack.NormalModuleReplacementPlugin(
+  /oidc-provider\/.*\/attention/,
+  path.join(__dirname, 'silent-attention.js')
+));
 
 // workaround for unsupported class properties in oidc-provider
 module.exports.module = {
