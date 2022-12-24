3c22332e85399a0c857d83c675acbd1e8617db90
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-12-24 16:15:51 +0100

    Update scalatest to 3.2.14 (#77)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- project/Deps.scala
+++ project/Deps.scala
@@ -4,7 +4,7 @@ import sbt._
 object Deps {
   import Def.{setting => s}
 
-  val scalatest = s("org.scalatest" %%% "scalatest" % "3.2.13")
+  val scalatest = s("org.scalatest" %%% "scalatest" % "3.2.14")
 
   val cats = new {
     val core   = s("org.typelevel" %%% "cats-core" % "2.8.0")
