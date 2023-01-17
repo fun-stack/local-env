012bef654bbb3cab977d65210ae595aed27c8dce
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-01-17 17:47:17 +0100

    Update cats-effect to 3.4.5 (#87)

--- project/Deps.scala
+++ project/Deps.scala
@@ -8,7 +8,7 @@ object Deps {
 
   val cats = new {
     val core   = s("org.typelevel" %%% "cats-core" % "2.9.0")
-    val effect = s("org.typelevel" %%% "cats-effect" % "3.4.4")
+    val effect = s("org.typelevel" %%% "cats-effect" % "3.4.5")
     val alley  = s("org.typelevel" %%% "alleycats-core" % "2.9.0")
   }
 
