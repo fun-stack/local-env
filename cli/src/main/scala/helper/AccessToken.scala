ee5ad535b994281781d2008dafb4e99789c1439d
johannes karoff <johannes@karoff.net>
2023-02-02 13:23:42 +0100

    stringify and cleanup accesstoken claims of authorizer

--- cli/src/main/scala/helper/AccessToken.scala
+++ cli/src/main/scala/helper/AccessToken.scala
@@ -7,19 +7,22 @@ import scala.scalajs.js
 object AccessToken {
   def toAuthorizer(accessToken: Option[String]): js.Dynamic = accessToken match {
     case None              =>
-      js.Dynamic.literal(
-        principalId = "anon",
-      )
+      js.Dynamic.literal()
     case Some(accessToken) =>
-      js.Object
-        .assign(
-          js.Dynamic
-            .literal(
-              principalId = "user",
-            )
-            .asInstanceOf[js.Object],
-          jwt_decode[JwtPayload](accessToken),
-        )
-        .asInstanceOf[js.Dynamic]
+      val jwtPayload = jwt_decode[JwtPayload](accessToken)
+      stringifyClaims(jwtPayload).asInstanceOf[js.Dynamic]
+  }
+
+  private def stringifyClaims(jwtPayload: JwtPayload): js.Object = {
+    val stringEntries = js.Object.entries(jwtPayload).map { case js.Tuple2(key, value) =>
+      if (js.typeOf(value) == "string" || value.isInstanceOf[String]) {
+        js.Tuple2(key, value)
+      }
+      else {
+        js.Tuple2(key, js.JSON.stringify(value.asInstanceOf[js.Any]))
+      }
+    }
+
+    js.Object.fromEntries(stringEntries).asInstanceOf[js.Object]
   }
 }
