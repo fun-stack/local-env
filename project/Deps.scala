a7df835a7b129814d60581afb07181d9339f1410
Scala Steward <scala_steward@virtuslab.com>
2024-01-12 20:25:52 +0000

    Update alleycats-core, cats-core to 2.10.0

--- project/Deps.scala
+++ project/Deps.scala
@@ -7,9 +7,9 @@ object Deps {
   val scalatest = s("org.scalatest" %%% "scalatest" % "3.2.15")
 
   val cats = new {
-    val core   = s("org.typelevel" %%% "cats-core" % "2.9.0")
+    val core   = s("org.typelevel" %%% "cats-core" % "2.10.0")
     val effect = s("org.typelevel" %%% "cats-effect" % "3.4.7")
-    val alley  = s("org.typelevel" %%% "alleycats-core" % "2.9.0")
+    val alley  = s("org.typelevel" %%% "alleycats-core" % "2.10.0")
   }
 
   val scalaJS = new {
